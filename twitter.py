d57ebb22ad5ebfd2c40a49dbc0cb325fac531586
arun <avisingh10.as@gmail.com>
2020-07-02 05:02:52 -0700

    add function to likes your followers recent posts

--- twitter.py
+++ twitter.py
@@ -51,5 +51,42 @@ def twitter_bot():
 
 
 n=twitter_bot()
-print(n)
 
+'''Function to get your follower and likes his recent no. of posts'''
+
+def get_followers():
+    username = input("enter your username : ")
+    followers_list = []
+    no_of_tweets = 3
+    count = 0
+    count_loop = 0
+    likes = 0
+
+
+    for follower in tweepy.Cursor(api.followers,screen_name=username).items():
+        followers_list.append(follower.screen_name)
+        count_loop  +=1
+
+
+        for tweet in tweepy.Cursor(api.user_timeline, screen_name=followers_list[0], tweet_mode="extended").items(no_of_tweets):
+            count +=1
+            if count == 1:
+                followers_list.pop()
+            try:
+                print('tweet liked')
+                likes += 1
+                print(count_loop)
+                print(likes,'tweet_liked')
+                # print(tweet.screen_name)
+
+                tweet.favorite()
+                time.sleep(1)
+            except tweepy.TweepError as e:
+                print(e.reason)
+            except StopIteration:
+                break
+
+
+
+
+print(get_followers())
